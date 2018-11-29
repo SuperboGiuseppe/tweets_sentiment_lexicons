30edd010dfd41e7db1d66cc4c158e9feccb0889a
Nicola Zotto <nzotto18@student.oulu.fi>
2018-11-29 13:26:03 +0100

    TweetTokenizer removes punctuation.
    TweetTokenzer.py now removes punctuation.

--- TweetTokenizer.py
+++ TweetTokenizer.py
@@ -4,9 +4,10 @@ Nicola Zotto
 Step 3: "Use TweetTokenizer package to tokenize the tweet messages and remove all links and special characters, and draw histogram of the most common terms, excluding stop-words."
 """
 import operator
+import string
 
-from nltk.corpus import stopwords
 # import nltk:
+from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
 
 # import custom modules:
@@ -22,11 +23,6 @@ def tokenizer_term_to_count(tweets, case_sensityvity=True, reduced_length=True, 
     :param reduced_length: if True, the tokenizer normalizes repeated characters to only 3 characters (default to True)
     :param delete_handles: if True, the tokenizer removes user handles such as "@Erme" (default to True)
     :return: a dictionary with a term as key and the number of appearances of the term as value
-
-    examples:
-    #>>> tweets = ["David Bowey is dead !!!!!!!!!!!!", "@A_Name LOOOOng live David bowey !"]
-    #>>> tokenizer_term_to_count(tweets) =={"david":2, "bowey": 2, "is":1, "dead":1, "!":4, "looong":1, "live":1}
-    True
     """
     tknzr = TweetTokenizer(preserve_case=case_sensityvity, strip_handles=delete_handles, reduce_len=reduced_length)
     res = dict()
@@ -34,9 +30,10 @@ def tokenizer_term_to_count(tweets, case_sensityvity=True, reduced_length=True, 
     for tw in tweets:
         temp = tknzr.tokenize(tw)
         filtered_tweets = [w for w in temp if w.lower() not in stopwords.words('english')]
-
         for token in filtered_tweets:
             if str(token).find('http') != -1:
+                continue
+            if token in string.punctuation:
                 continue
             if token in res.keys():
                 res[token] += 1
@@ -46,7 +43,7 @@ def tokenizer_term_to_count(tweets, case_sensityvity=True, reduced_length=True, 
 
 
 def main():
-    # tweets = ["David Bowey is dead !!!!!!!!!!!!", "@A_Name LOOOOng live David bowey !"]
+##    tweets = ["David Bowey is dead !!!!!!!!!!!!", "@A_Name LOOOOng live David bowey !"]
     tweets_en = mdb.apply_query({}, {'_id': 0, 'text': 1}, collection_name='tweets_en')
     tweets_da = mdb.apply_query({}, {'_id': 0, 'translated_text': 1}, collection_name='tweets_da')
     tweets_fi = mdb.apply_query({}, {'_id': 0, 'translated_text': 1}, collection_name='tweets_fi')
