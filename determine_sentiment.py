e98b667104a378c32fc7db3a7a7ffd75b300f91f
geuek <38188823+geuek@users.noreply.github.com>
2018-11-30 13:53:31 +0200

    something didn't work. see earlier commit message

--- determine_sentiment.py
+++ determine_sentiment.py
@@ -5,29 +5,43 @@ import csv
 
 
 def read_sentistrength_file(file_name):
-    with open(file_name, 'r', encoding='utf-8') as result_file:
-        reader = csv.reader(result_file, delimiter='\t')
-        # headers = next(reader, None)
-        next(reader)
-        negative = 0
-        positive = 0
-        count = 0
-        for row in reader:
-            positive = positive + int(row[2])
-            negative = negative + int(row[3])
-            count = count + 1
-        positive = positive / count
-        negative = negative / count
-    result_file.close()
-    return positive, negative
+    try:
+        with open(file_name, 'r', encoding='utf-8') as result_file:
+            reader = csv.reader(result_file, delimiter='\t')
+            next(reader)
+            negative = 0
+            positive = 0
+            count = 0
+            for row in reader:
+                positive = positive + int(row[2])
+                negative = negative + int(row[3])
+                count = count + 1
+            positive = positive / count
+            negative = negative / count
+        result_file.close()
+        return positive, negative
+    except FileNotFoundError as fnf:
+        print(file_name, fnf.strerror)
+        return -1, -1
 
 
 def main():
-    pos, neg = read_sentistrength_file('/textfiles/placeholder_file.txt')
-    print('Positive Value:')
-    print(pos)
-    print('Negative Value:')
-    print(neg)
+    pos_da, neg_da = read_sentistrength_file('textfiles/tweets_da+results.txt')
+    pos_en, neg_en = read_sentistrength_file('textfiles/tweets_en+results.txt')
+    pos_no, neg_no = read_sentistrength_file('textfiles/tweets_no+results.txt')
+    pos_fi, neg_fi = read_sentistrength_file('textfiles/tweets_fi+results.txt')
+    pos_sv, neg_sv = read_sentistrength_file('textfiles/tweets_sv+results.txt')
+
+    print("English:\nPositive: ", pos_en, "\tNegative: ", neg_en)
+    print()
+    print("Finnish:\nPositive: ", pos_fi, "\tNegative: ", neg_fi)
+    print()
+    print("Danish:\nPositive: ", pos_da, "\tNegative: ", neg_da)
+    print()
+    print("Norwegian:\nPositive: ", pos_no, "\tNegative: ", neg_no)
+    print()
+    print("Swedish:\nPositive: ", pos_sv, "\tNegative: ", neg_sv)
 
 
 if __name__ == "__main__": main()
+
