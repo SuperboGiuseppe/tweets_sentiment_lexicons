c55730624e7978264c41fbc03e98057af888f235
ulmanov <emre-arkan@hotmail.com>
2019-01-06 16:43:46 +0100

    -add function to extract the average sentiment score of a user in most_active_users.py -result is now ignored if equal to 0 in empath

--- most_active_users.py
+++ most_active_users.py
@@ -6,14 +6,32 @@ __copyright__ = "Copyright 2018, Project for Natural Language Processing and Tex
 __email__ = "emrearkan@outlook.de"
 
 import operator
+import os
+import sys
+from contextlib import contextmanager
 
 import mongodb_functions as mdb
 from insert_tweets import filter_dict
 
 
+@contextmanager
+def suppress_stdout():
+    """
+    to suppress some console outputs
+    found on https://gist.github.com/djsmith42/3956189#file-gistfile1-py
+    """
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
 def find_x_most_active_users(collection_name, db_name='tweets', num_of_users=None, filter={}):
     """
-    Sorts users by tweet number in a descending order in a given tweet collection and return the given most active users
+    Sorts users by tweet number in a descending order in a given tweet collection and returns the given most active users
     :param collection_name: collection to be opened
     :param db_name: database to be connected to
     :param num_of_users: limits the number of returned users, if not passed there is no limit
@@ -32,6 +50,40 @@ def find_x_most_active_users(collection_name, db_name='tweets', num_of_users=Non
     return dict((x, y) for x, y in desc_users[:num_of_users])
 
 
+def avg_sentiment_of_active_user(user_id, collection_name, db_name='tweets'):
+    """
+    Returns average sentiment values for given user
+    :param user_id: id of the user, whose tweets are being analysed
+    :param collection_name: collection to be opened
+    :param db_name: database to be connected to
+    :return: dict of the average sentiment scores of the given user
+    """
+    with suppress_stdout():
+        senti_strength = mdb.apply_query({'user.id': user_id}, {'_id': 0, 'SentiStrength': 1},
+                                         collection_name=collection_name)
+        senti_strength = [val['SentiStrength'] for val in senti_strength]
+
+        senti_word_net = mdb.apply_query({'user.id': user_id}, {'_id': 0, 'SentiWordNet': 1},
+                                         collection_name=collection_name)
+        senti_word_net = [val['SentiWordNet'] for val in senti_word_net]
+
+        text_blob = mdb.apply_query({'user.id': user_id}, {'_id': 0, 'Textblob': 1}, collection_name=collection_name)
+        text_blob = [val['Textblob'][0] for val in text_blob]
+
+    senti_strength_avg = 0
+    senti_word_net_avg = 0
+    text_blob_avg = 0
+    for i in range(0, len(senti_strength)):
+        senti_strength_avg += senti_strength[i]
+        senti_word_net_avg += senti_word_net[i]
+        text_blob_avg += text_blob[i]
+    senti_strength_avg /= len(senti_strength)
+    senti_word_net_avg /= len(senti_word_net)
+    text_blob_avg /= len(text_blob)
+    return {'senti_strength_avg': senti_strength_avg, 'senti_word_net_avg': senti_word_net_avg,
+            'text_blob_avg': text_blob_avg}
+
+
 def main():
     collection_names = [key for key in filter_dict.keys()]
     num_of_users = 2
@@ -47,7 +99,11 @@ def main():
         print(num_of_users, 'most active users in', collection_name, 'are:')
         i = 1
         for key in users.keys():
-            print('\t', '{}.'.format(i), 'User ID:', key, 'with', users[key], 'tweets.')
+            print('\t', '{}.'.format(i), 'User ID:', key, 'with', users[key], 'tweets:')
+            sentiments = avg_sentiment_of_active_user(key, collection_name=collection_name)
+            print('\t\t', 'Average sentiment score of SentiStrength = {}'.format(sentiments['senti_strength_avg']))
+            print('\t\t', 'Average sentiment score of SentiWordNet = {}'.format(sentiments['senti_word_net_avg']))
+            print('\t\t', 'Average sentiment score of Textblob = {}'.format(sentiments['text_blob_avg']))
             i += 1
         print()
 
